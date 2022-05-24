a7ba07c3fabb9c30b9f7e006861abc975a79786e
Brian M. Viana <brianmviana@gmail.com>
2022-05-24 19:27:05 -0300

    initialing project

---
+++ src/main/kotlin/dev/brianmviana/hexaarch/netflixhexagonalarchitecture/NetflixHexagonalArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package dev.brianmviana.hexaarch.netflixhexagonalarchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NetflixHexagonalArchitectureApplication
+
+fun main(args: Array<String>) {
+	runApplication<NetflixHexagonalArchitectureApplication>(*args)
+}
---
+++ src/test/kotlin/dev/brianmviana/hexaarch/netflixhexagonalarchitecture/NetflixHexagonalArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.brianmviana.hexaarch.netflixhexagonalarchitecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NetflixHexagonalArchitectureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
